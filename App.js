8e0ea4c699d3f297e9bc6baaf35fd201b1575640
keeperofthescum <faisalaldoraihem@gmail.com>
2019-06-14 15:48:51 +0300

    Added screen nav
    Screen nav with react-navigation is now a go-go

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, YellowBox } from 'react-native';
+import HomeScreen from './screens/HomeActivity'
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+import MainActivity from './screens/MainActivity';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const RootStack = createStackNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Main: { screen: MainActivity },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <AppContainer
+        ref={nav => {
+          this.navigator = nav;
+        }}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
